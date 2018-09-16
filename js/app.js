eaac51d878ba77afd1b239f8ac3bdce31ffb1af1
Megan Bailey <mkbailey391@gmail.com>
2018-09-16 14:20:06 -0700

    volume adjust code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-(function() {
-    // Write code here
-})()
+console.log('JS loaded')
+
+function volumeChange(){
+    let input=document.getElementById('start');
+    input.addEventListener('change', function(elem){
+    document.getElementById('volume-level').innerHTML=this.value;
+    })
+}
+volumeChange();
